cdcb0e5ce6ef0b2f2608362a8dfd5aedfdadf916
Kirill Khudyakov <hudiakov@gmail.com>
2020-09-13 14:00:38 +0300

    [+] pods init

---
+++ SberbankConcept/SberbankConcept/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SberbankConcept
+//
+//  Created by Кирилл Худяков on 13.09.2020.
+//  Copyright © 2020 Худяков Кирилл. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
---
+++ SberbankConcept/SberbankConcept/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SberbankConcept
+//
+//  Created by Кирилл Худяков on 13.09.2020.
+//  Copyright © 2020 Худяков Кирилл. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
